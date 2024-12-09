8fb9a07cd45a4ec4422b4f2d9083fd4a7720a80f
rmichiels <Microbbe+clean@gmail.com>
2024-12-09 16:55:33 +0100

    - First Pass on reactiveproperty - First Pass on Recipe Steps - Various UI Fixes

---
+++ Enplace/Enplace/Enplace.Library/Context/ReactiveProperty.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enplace.Library.Context
+{
+    public class ReactiveProperty<T>
+    {
+        //public delegate Task AsyncEventHandler(T? message);
+        public delegate void AsyncEventHandler(T? input);
+        public event AsyncEventHandler _trigger;
+        private T _value;
+
+        public void Bind(params AsyncEventHandler[] args)
+        {
+            foreach (var handler in args)
+            {
+                _trigger += handler;
+            }
+        }
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                _trigger?.Invoke(value);
+            }
+        }
+
+        public ReactiveProperty(T value)
+        {
+            Value = value;
+        }
+    }
+}
